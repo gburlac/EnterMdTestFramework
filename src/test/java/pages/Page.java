0c942946aec06f5f8fa2b2c1ade458cdd1e69063
vatamanenco <Ogogo761967>
2019-12-14 16:19:17 +0200

    UI testcase started

--- src/test/java/pages/Page.java
+++ src/test/java/pages/Page.java
@@ -4,7 +4,49 @@ import org.openqa.selenium.support.PageFactory;
 import util.Driver;
 
 public class Page {
-    Page() {
+    public Page() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    MainPage mainPage;
+    CadouPentruEa cadouPentruEa;
+    CartPage cartPage;
+    LoginPopup loginPopup;
+    MainPageAsLogged mainPageAsLogged;
+    ProductPage productPage;
+    Smartphones smartphones;
+    SmartphonesCategory smartphonesCategory;
+    ToateCategoriile toateCategoriile;
+    Electrocasnice electrocasnice;
+
+    public MainPage getMainPage() {
+        return (mainPage == null) ? mainPage = new MainPage() : mainPage;
+    }
+
+    public CadouPentruEa getCadouPentruEa() {
+        return (cadouPentruEa == null) ? cadouPentruEa = new CadouPentruEa() : cadouPentruEa; }
+
+    public CartPage getCartPage() {
+        return (cartPage == null) ? cartPage = new CartPage() : cartPage; }
+
+    public LoginPopup getLoginPopup() {
+        return (loginPopup == null) ? loginPopup = new LoginPopup() : loginPopup; }
+
+    public MainPageAsLogged getMainPageAsLogged() {
+        return (mainPageAsLogged == null) ? mainPageAsLogged = new MainPageAsLogged() : mainPageAsLogged; }
+
+    public ProductPage getProductPage() {
+        return (productPage == null) ? productPage = new ProductPage() : productPage; }
+
+    public Smartphones getSmartphones() {
+        return (smartphones == null) ? smartphones = new Smartphones() : smartphones; }
+
+    public SmartphonesCategory getSmartphonesCategory() {
+        return (smartphonesCategory == null) ? smartphonesCategory = new SmartphonesCategory() : smartphonesCategory; }
+
+    public ToateCategoriile toateCategoriile() {
+        return (toateCategoriile == null) ? toateCategoriile = new ToateCategoriile() : toateCategoriile; }
+
+    public Electrocasnice getElectrocasnice() {
+        return (electrocasnice == null) ? electrocasnice = new Electrocasnice() : electrocasnice; }
 }
